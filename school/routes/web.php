e49620b65dfcdd294847361b67b59dbeaa3a7b06
Winston Cahya <winstoncahya90@gmail.com>
2021-07-05 08:48:20 +0800

    Fixed Displaying and Students database are shown

--- school/routes/web.php
+++ school/routes/web.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/students', 'StudentsController@index')-> middleware('auth');
 
 Auth::routes();
 
